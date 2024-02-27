c62d36519d82cf61e0a3e29c966558142de36348
Zaver85 <zaver85.com@maol.ru>
2024-02-27 20:23:27 +0300

    A simple calculator

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args){
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите 1-ое число: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Введите 2-ое число: ");
+        int num2 = scanner.nextInt();
+
+        int res;
+
+        System.out.print("Действие: ");
+        String action = scanner.nextLine();
+        action = scanner.nextLine();
+
+        switch (action) {
+            case  "+":
+                res = num1 + num2;
+                System.out.println("Результат " + res);
+                break;
+            case  "-":
+                res = num1 - num2;
+                System.out.println("Результат " + res);
+                break;
+            case  "*":
+                res = num1 * num2;
+                System.out.println("Результат " + res);
+                break;
+            case  "/":
+                if (num2 == 0)
+                    System.out.println("Error");
+                else {
+                    res = num1 / num2;
+                    System.out.println("Результат " + res);
+                }
+                break;
+            default:
+                System.out.println("Вы что-то не то ввели");
+        }
+    }
+}
